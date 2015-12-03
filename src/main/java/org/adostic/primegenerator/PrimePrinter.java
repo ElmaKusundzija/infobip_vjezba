d04232e5bff6a5fa9b82c854356b14406c262546
ElmaKusundzija <ekusundzij1@etf.unsa.ba>
2015-12-03 01:23:01 +0100

    Urađen prvi mali refaktoring

--- src/main/java/org/adostic/primegenerator/PrimePrinter.java
+++ src/main/java/org/adostic/primegenerator/PrimePrinter.java
@@ -3,66 +3,65 @@ package org.adostic.primegenerator;
 public class PrimePrinter {
 
 	public static void main(String[] args) {
-		final int M = 1000;
+		final int THOUSAND = 1000;
 		final int RR = 50;
 		final int CC = 4;
 		final int WW = 10;
 		final int ORDMAX = 30;
-		int P[] = new int[M + 1];
-		int PAGENUMBER;
-		int PAGEOFFSET;
-		int ROWOFFSET;
-		int C;
-		int J;
-		int K;
-		boolean JPRIME;
-		int ORD;
-		int SQUARE;
-		int N;
-		int MULT[] = new int[ORDMAX + 1];
-		J = 1;
-		K = 1;
-		P[1] = 2;
-		ORD = 2;
-		SQUARE = 9;
-		while (K < M) {
+
+		int prostiBrojevi[] = new int[THOUSAND + 1];
+		int mult[] = new int[ORDMAX + 1];
+
+		int trenutniProstiBroj = 1;
+		int brojPronadenihProstihBrojeva=1;
+		prostiBrojevi[1] = 2;
+		int squere = 9;
+		boolean jPrime=true;
+		int ord = 2;
+
+		while (brojPronadenihProstihBrojeva < THOUSAND) {
 			do {
-				J = J + 2;
-				if (J == SQUARE) {
-					ORD = ORD + 1;
-					SQUARE = P[ORD] * P[ORD];
-					MULT[ORD - 1] = J;
+				trenutniProstiBroj = getIduciProstiBroj(trenutniProstiBroj);
+				if (trenutniProstiBroj == squere) {
+					ord = ord + 1;
+					squere = prostiBrojevi[ord] * prostiBrojevi[ord];
+					mult[ord - 1] = trenutniProstiBroj;
 				}
-				N = 2;
-				JPRIME = true;
-				while (N < ORD && JPRIME) {
-					while (MULT[N] < J)
-						MULT[N] = MULT[N] + P[N] + P[N];
-					if (MULT[N] == J)
-						JPRIME = false;
+				int N = 2;
+				jPrime = true;
+				while (N < ord && jPrime) {
+					while (mult[N] < trenutniProstiBroj) {
+						mult[N] = mult[N] + prostiBrojevi[N] + prostiBrojevi[N];
+					}
+					if (mult[N] == trenutniProstiBroj)
+						jPrime = false;
 					N = N + 1;
 				}
-			} while (!JPRIME);
-			K = K + 1;
-			P[K] = J;
+			} while (!jPrime);
+			brojPronadenihProstihBrojeva += 1;
+			prostiBrojevi[brojPronadenihProstihBrojeva] = trenutniProstiBroj;
 		}
 		{
-			PAGENUMBER = 1;
-			PAGEOFFSET = 1;
-			while (PAGEOFFSET <= M) {
-				System.out.println("The First " + M +
-						" Prime Numbers --- Page " + PAGENUMBER);
+			int pageNumber = 1;
+			int pageOffset = 1;
+			while (pageOffset <= THOUSAND) {
+				System.out.println("The First " + THOUSAND +
+						" Prime Numbers --- Page " + pageNumber);
 				System.out.println("");
-				for (ROWOFFSET = PAGEOFFSET; ROWOFFSET < PAGEOFFSET + RR; ROWOFFSET++){
-					for (C = 0; C < CC;C++)
-						if (ROWOFFSET + C * RR <= M)
-							System.out.format("%10d", P[ROWOFFSET + C * RR]);
+				for (int rowOffset = pageOffset; rowOffset < pageOffset + RR; rowOffset++){
+					for (int C = 0; C < CC;C++)
+						if (rowOffset + C * RR <= THOUSAND)
+							System.out.format("%10d", prostiBrojevi[rowOffset + C * RR]);
 					System.out.println("");
 				}
 				System.out.println("\f");
-				PAGENUMBER = PAGENUMBER + 1;
-				PAGEOFFSET = PAGEOFFSET + RR * CC;
+				pageNumber = pageNumber + 1;
+				pageOffset = pageOffset + RR * CC;
 			}
 		}
+	}
+
+	private static int getIduciProstiBroj(int trenutniProstiBroj) {
+		return trenutniProstiBroj+2;
 	}
 }
